cc1980fdb6b864c90dc2514e0c4254646834e99a
Lor-Antony <lor@et.esiea.fr>
2017-12-18 22:09:20 +0100

    Loading screen have know a loading bar progress

--- Assets/Scripts/MenuesScript/MapSelector.cs
+++ Assets/Scripts/MenuesScript/MapSelector.cs
@@ -2,12 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MapSelector : MonoBehaviour {
 
-	public void LoadScene(int levelID){
-	
-		SceneManager.LoadSceneAsync(levelID);
+	public GameObject LoadingImage;
+	public Slider loadingBar;
+	private AsyncOperation async;
 
+
+	//set the loading screen and call loadlevelwith bar
+	public void SetLoadingScreen(int levelID){
+		LoadingImage.SetActive (true);
+		StartCoroutine (LoadLevelWithBar (levelID));
+	}
+
+
+	//asynchronously call the new scene and show the progress on the bar
+	IEnumerator LoadLevelWithBar (int levelID){
+
+		async=	SceneManager.LoadSceneAsync(levelID);
+		while (!async.isDone) {
+			loadingBar.value = async.progress;
+			yield return null;
+		}
 	}
 }
+
