0d79536de434692da53e3ecd58121244b0b72df6
law0 <lawrencio@outlook.fr>
2018-03-04 21:25:20 +0100

    commentaires

--- Assets/Scripts/CommonScript/MatchManager.cs
+++ Assets/Scripts/CommonScript/MatchManager.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+
+
+/*
+ * J'ai fait ce script un peu vite fait, mais ca marche bien
+ * MatchManager répartit les joueurs en 2 équipe, un peu basiquement pour l'instant (toi dans une, toi dans l'autre, toi dans l'une...)
+ * Il gere aussi (surtout) "le début du match"
+ * Basiquement on fait respawn tout le monde pour l'instant
+ * */
 public class MatchManager : NetworkBehaviour
 {
 	public bool hasTeams = true;
@@ -12,13 +20,13 @@ public class MatchManager : NetworkBehaviour
 
 	private float startTime = -1.0F;
 
-	[SyncVar]
+	[SyncVar] //alors oui j'ai dit "JAMAIS de SyncVar!" mais il la... c'est juste pour afficher le délai...
 	public float delayBeforeStart = -1.0F;
 
 	public GameObject teamASpawnPoint = null;
 	public GameObject teamBSpawnPoint = null;
 
-	[SyncVar]
+	[SyncVar] //et la pour dire que le match a démarré...
 	private bool matchStarted = false;
 	public bool MatchStarted
 	{
@@ -27,8 +35,8 @@ public class MatchManager : NetworkBehaviour
 			return matchStarted;
 		}
 	}
-
-	[SyncVar]
+	 
+	[SyncVar] //Bon d'accord je l'utilise beaucoup ici! Mais SyncVar est pratique pour des trucs simples
 	private bool matchStartedDelay = false;
 	public bool MatchStartedDelay
 	{
@@ -57,6 +65,7 @@ public class MatchManager : NetworkBehaviour
 		if (!isServer)
 			return;
 
+		//faire bien attention a ce que startTime soit inférieur à 0 à l'instantiation
 		if (startTime > 0.0F && startTime < Time.time && ! matchStarted)
 		{
 			if (hasTeams)
@@ -118,6 +127,7 @@ public class MatchManager : NetworkBehaviour
 		}
 	}
 
+	//il faut que chaque client ajoute les joueurs dans la bonne equipe dans leur version du jeu
 	[ClientRpc]
 	private void RpcAddToTeamA(int index)
 	{
